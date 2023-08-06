2d9cc3d7f3e0a9262e7be61ed276ca8c637560d0
Anthony Rende <rende718@gmail.com>
2023-08-06 00:08:06 -0400

    feat: fetch coin details to display in CoinStats component

--- src/components/CoinStats/index.tsx
+++ src/components/CoinStats/index.tsx
@@ -1,26 +1,30 @@
 import {
   Box,
+  Button,
   chakra,
   Flex,
+  Img,
   SimpleGrid,
   Stat,
   StatLabel,
   StatNumber,
   useColorModeValue,
-} from '@chakra-ui/react'
-import { ReactNode } from 'react'
-import { BsPerson } from 'react-icons/bs'
-import { FiServer } from 'react-icons/fi'
-import { GoLocation } from 'react-icons/go'
+} from '@chakra-ui/react';
+import { ReactNode } from 'react';
+import { BsPerson } from 'react-icons/bs';
+import { FiServer } from 'react-icons/fi';
+import { GoLocation } from 'react-icons/go';
+
+import useFetchCoinDetails from '@/hooks/useFetchCoinDetails';
 
 interface StatsCardProps {
-  title: string
-  stat: string
-  icon: ReactNode
+  title: string;
+  stat: string | JSX.Element;
+  icon: ReactNode;
 }
 
 function StatsCard(props: StatsCardProps) {
-  const { title, stat, icon } = props
+  const { title, stat, icon } = props;
   return (
     <Stat
       px={{ base: 2, md: 4 }}
@@ -28,7 +32,8 @@ function StatsCard(props: StatsCardProps) {
       shadow={'xl'}
       border={'1px solid'}
       borderColor={useColorModeValue('gray.800', 'gray.500')}
-      rounded={'lg'}>
+      rounded={'lg'}
+    >
       <Flex justifyContent={'space-between'}>
         <Box pl={{ base: 2, md: 4 }}>
           <StatLabel fontWeight={'medium'} isTruncated>
@@ -41,25 +46,53 @@ function StatsCard(props: StatsCardProps) {
         <Box
           my={'auto'}
           color={useColorModeValue('gray.800', 'gray.200')}
-          alignContent={'center'}>
+          alignContent={'center'}
+        >
           {icon}
         </Box>
       </Flex>
     </Stat>
-  )
+  );
 }
 
-export default function BasicStatistics() {
+export default function CoinStats(coins) {
+  const { coinsWithPrices, loading, error } = useFetchCoinDetails(coins);
+  console.log('coins withPrices: ', coinsWithPrices);
   return (
     <Box maxW="7xl" mx={'auto'} pt={5} px={{ base: 2, sm: 12, md: 17 }}>
-      <chakra.h1 textAlign={'center'} fontSize={'4xl'} py={10} fontWeight={'bold'}>
-        Coin Stats
+      <chakra.h1
+        textAlign={'center'}
+        fontSize={'4xl'}
+        py={10}
+        fontWeight={'bold'}
+      >
+        Coin Stats ({coins.coins.length})
       </chakra.h1>
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 5, lg: 8 }}>
-        <StatsCard title={'Users'} stat={'5,000'} icon={<BsPerson size={'3em'} />} />
-        <StatsCard title={'Servers'} stat={'1,000'} icon={<FiServer size={'3em'} />} />
-        <StatsCard title={'Datacenters'} stat={'7'} icon={<GoLocation size={'3em'} />} />
-      </SimpleGrid>
+      {coinsWithPrices.map((coin, index) => {
+        return (
+          <SimpleGrid
+            columns={{ base: 1, md: 3 }}
+            spacing={{ base: 5, lg: 8 }}
+            key={`${coins.coins.length}_${index}`}
+          >
+            <StatsCard
+              title={'Name'}
+              stat={coin.name}
+              icon={<Img src={coin.logoURI} boxSize={'3em'} />}
+            />
+            <StatsCard
+              title={'24h Price Change'}
+              stat={coin.v24hChangePercent.toFixed(2) + '%'}
+              icon={<FiServer size={'3em'} />}
+            />
+            <StatsCard
+              title={'Create Notification'}
+              stat={<Button colorScheme="purple">Create</Button>}
+              icon={<GoLocation size={'3em'} />}
+            />
+          </SimpleGrid>
+        );
+      })}
     </Box>
-  )
+  );
 }

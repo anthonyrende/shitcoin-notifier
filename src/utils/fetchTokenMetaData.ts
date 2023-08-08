8d7d5f537b8a9e3a43bec03cc99a7876e7fcc32f
Anthony Rende <rende718@gmail.com>
2023-08-08 18:36:14 -0400

    feat: create fetchTokenMetadta util function to fetch metadata by mint address

---
+++ src/utils/fetchTokenMetaData.ts
@@ -0,0 +1,45 @@
+import { Metaplex, toPublicKey } from '@metaplex-foundation/js';
+import { Metadata } from '@metaplex-foundation/mpl-token-metadata';
+import { Connection, clusterApiUrl } from '@solana/web3.js';
+
+/**
+ * Fetches metadata for a given mint address using the Metaplex library.
+ *
+ * @param {Connection} connection - The connection instance from the web3.js library.
+ * @param {string} mintAddress - The public key of the token's mint.
+ *
+ * @returns {Promise<Object>} - A Promise that resolves to an object containing the token's metadata.
+ *
+ * @throws Will throw an error if no account info is found at the address or if deserialization fails.
+ */
+
+export async function fetchTokenMetadata({ mintAddress }) {
+  try {
+    if (!mintAddress) {
+      return;
+    }
+    const url = `https://api.helius.xyz/v0/token-metadata?api-key=${process.env.NEXT_PUBLIC_HELIOUS_API_KEY}`;
+    const nftAddresses = [mintAddress];
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        mintAccounts: nftAddresses,
+        includeOffChain: true,
+        disableCache: false,
+      }),
+    });
+
+    const data = await response.json();
+    console.log('metadata: ', data);
+    return data[0];
+  } catch (error) {
+    console.error(
+      `Failed to fetch metadata for mint address ${mintAddress}: ${error}`,
+    );
+    throw error;
+  }
+}

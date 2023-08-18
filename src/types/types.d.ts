65e15d4fa6c4577fae6b43aca09ea09159d67fa6
Anthony Rende <rende718@gmail.com>
2023-08-18 00:18:19 -0400

    fix: update Coin interface

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -4,6 +4,20 @@ export interface Coin {
   amount: number;
   decimals: number;
   mint: string;
+  metaData: {
+    description: string;
+    image: string;
+    name: string;
+    symbol: string;
+    mint: string;
+  };
+  statsData: Array<{
+    mint: string;
+    name: string;
+    symbol: string;
+    decimals: number;
+  }>;
+  tokenBalance: number;
   priceData: {
     mints: string;
     price: number;
